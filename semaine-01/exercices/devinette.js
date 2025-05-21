525cfb58a75491dd790a99974ff8189f5d9e6a1a
sedik <sedik91270@hotmail.fr>
2025-05-21 15:35:49 +0200

    Ajout d'une amélioration dans le jeu devinette

--- semaine-01/exercices/devinette.js
+++ semaine-01/exercices/devinette.js
@@ -7,16 +7,31 @@ const motDeviner = mots[motAleatoire].toLowerCase();
 
 let motCacher = "_".repeat(motDeviner.length).split("");
 
-while (motCacher.includes("_")) {
+let essaiRestant = 6;
+
+while (motCacher.includes("_") && essaiRestant > 0) {
   let chercher = prompt("Quelle lettre choisi(e)-tu ? : ").toLowerCase();
+  let trouver = false;
 
   console.log("Lettre cherchée :", chercher);
   for (let i = 0; i < motDeviner.length; i++) {
     if (chercher === motDeviner[i]) {
       motCacher[i] = chercher;
+      trouver = true;
     }
+  }
+
+  if (!trouver) {
+    essaiRestant--;
+    console.log("❌ Mauvaise lettre !");
+    console.log("Mot : " + motCacher.join(" "));
+    console.log(`🧪 Essais restants : ${essaiRestant}`);
   }
   console.log(motCacher.join(" "));
 }
 
-// console.log(motCacher.join(" "));
+if (!motCacher.includes("_")) {
+  console.log("🎉 Bravo ! Tu as trouvé le mot : " + motDeviner);
+} else {
+  console.log("💀 Perdu ! Le mot était : " + motDeviner);
+}
