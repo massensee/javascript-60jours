d693183fbf6c1a30bbec47764eed10da44879b8f
sedik <sedik91270@hotmail.fr>
2025-05-24 19:03:37 +0200

    Ajout du formulaire d'inscription avec mise en forme CSS et validation de base

--- semaine-02/site-interactif/script.js
+++ semaine-02/site-interactif/script.js
@@ -3,6 +3,7 @@ const menuBurger = document.getElementById("menu-burger");
 const nav = document.querySelector("nav");
 const btnToggle = document.getElementById("toggle-apropos");
 const textApropos = document.getElementById("texte-apropos");
+const form = document.querySelector("form");
 
 menuBurger.addEventListener("click", () => {
   nav.classList.toggle("visible");
@@ -18,4 +19,11 @@ btnToggle.addEventListener("click", () => {
   const estCacher = textApropos.classList.contains("cacher");
 
   btnToggle.textContent = estCacher ? "Voir plus" : "Voir moins";
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const prenom = document.getElementById("prenom").value;
+  alert(`Merci ${prenom} ! Votre message a bien été envoyé.`);
 });
