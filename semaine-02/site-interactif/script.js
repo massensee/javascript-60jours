cfbf1b5a9fae5e2109ecbca856e3f9ac2311d0f3
sedik <sedik91270@hotmail.fr>
2025-05-24 11:29:51 +0200

    Ajout d'un menu burger

--- semaine-02/site-interactif/script.js
+++ semaine-02/site-interactif/script.js
@@ -1,5 +1,21 @@
 const btn = document.getElementById("changer-couleur");
+const menuBurger = document.getElementById("menu-burger");
+const nav = document.querySelector("nav");
+const btnToggle = document.getElementById("toggle-apropos");
+const textApropos = document.getElementById("texte-apropos");
+
+menuBurger.addEventListener("click", () => {
+  nav.classList.toggle("visible");
+});
 
 btn.addEventListener("click", function () {
   document.body.classList.toggle("lightBlue");
+});
+
+btnToggle.addEventListener("click", () => {
+  textApropos.classList.toggle("cacher");
+
+  const estCacher = textApropos.classList.contains("cacher");
+
+  btnToggle.textContent = estCacher ? "Voir plus" : "Voir moins";
 });
