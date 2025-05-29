4e8f92baaf53835ba8c37fffb2723133bb3f4616
sedik <sedik91270@hotmail.fr>
2025-05-29 11:01:21 +0200

    Ajout du bouton de suppression pour chaque tâche avec style personnalisé

--- semaine-02/todo-list/script.js
+++ semaine-02/todo-list/script.js
@@ -1,1 +1,28 @@
+const btnAddTask = document.getElementById("ajouter");
+const result = document.getElementById("liste-taches");
+const task = document.getElementById("tache");
 
+btnAddTask.addEventListener("click", () => {
+  if (task.value === "") {
+    alert("Veuillez entrer une tache !");
+  } else {
+    addTask(task.value);
+    task.value = "";
+  }
+});
+
+function addTask(text) {
+  const liEl = document.createElement("li");
+  liEl.textContent = text;
+
+  const deleteTask = document.createElement("button");
+  deleteTask.classList.add("btn-supprimer");
+  deleteTask.textContent = "❌";
+
+  deleteTask.addEventListener("click", (e) => {
+    e.stopPropagation();
+    liEl.remove();
+  });
+  result.appendChild(liEl);
+  liEl.appendChild(deleteTask);
+}
