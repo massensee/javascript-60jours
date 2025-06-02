42ffe988dda570dbe757ccd1184a8457a8d9121f
sedik <sedik91270@hotmail.fr>
2025-06-02 22:15:19 +0200

    Exercices tableau filter, forEach, map

--- semaine-03/exercice-tab/script.js
+++ semaine-03/exercice-tab/script.js
@@ -20,15 +20,15 @@
 
 // console.log(nomEnMajuscule);
 
-const produits = [
-  { nom: "Stylo", prix: 1.5, disponible: true },
-  { nom: "Cahier", prix: 3.0, disponible: false },
-  { nom: "Gomme", prix: 1.9, disponible: false },
-  { nom: "Effaceur", prix: 2.1, disponible: true },
-  { nom: "Règle", prix: 2.4, disponible: true },
-  { nom: "Livre", prix: 2.8, disponible: true },
-  { nom: "Trousse", prix: 2.6, disponible: false },
-];
+// const produits = [
+//   { nom: "Stylo", prix: 1.5, disponible: true },
+//   { nom: "Cahier", prix: 3.0, disponible: false },
+//   { nom: "Gomme", prix: 1.9, disponible: false },
+//   { nom: "Effaceur", prix: 2.1, disponible: true },
+//   { nom: "Règle", prix: 2.4, disponible: true },
+//   { nom: "Livre", prix: 2.8, disponible: true },
+//   { nom: "Trousse", prix: 2.6, disponible: false },
+// ];
 
 // let taxer = [];
 
@@ -51,14 +51,45 @@ const produits = [
 
 // console.log(produitTaxer);
 
-function ajouterTva(produits) {
-  let nouveauTableau = [];
-  produits.forEach((element) => {
-    const produit = element.prix * 1.2;
-    nouveauTableau.push(produit);
-  });
-  return nouveauTableau;
-}
+// function ajouterTva(produits) {
+//   let nouveauTableau = [];
+//   produits.forEach((element) => {
+//     const produit = element.prix * 1.2;
+//     nouveauTableau.push(produit);
+//   });
+//   return nouveauTableau;
+// }
 
-const resultat = ajouterTva(produits);
-console.log(resultat);
+// const resultat = ajouterTva(produits);
+// console.log(resultat);
+
+// function ajouterTvaDetaillee(produits) {
+//   const tableauObj = [];
+
+//   produits.forEach((element) => {
+//     const produit = element.prix * 1.2;
+
+//     const obj = {
+//       nom: element.nom,
+//       prixOriginal: element.prix,
+//       prixTTC: produit.toFixed(2),
+//     };
+//     tableauObj.push(obj);
+//   });
+
+//   return tableauObj;
+// }
+
+// const resultat = ajouterTvaDetaillee(produits);
+
+// console.log(resultat);
+
+const produits = [
+  { nom: "Stylo", prix: 1.5, quantite: 430, disponible: true },
+  { nom: "Cahier", prix: 3.0, quantite: 220, disponible: false },
+  { nom: "Gomme", prix: 1.9, quantite: 350, disponible: false },
+  { nom: "Effaceur", prix: 2.1, quantite: 420, disponible: true },
+  { nom: "Règle", prix: 2.4, quantite: 300, disponible: true },
+  { nom: "Livre", prix: 2.8, quantite: 500, disponible: true },
+  { nom: "Trousse", prix: 2.6, quantite: 360, disponible: false },
+];
