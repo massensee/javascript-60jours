760b4cb51ee68e68c1fb07a789048d6f7bde899a
sedik <sedik91270@hotmail.fr>
2025-06-03 18:00:02 +0200

    Ajout du dashboard exercice boucle

--- semaine-03/exercice-tab/script.js
+++ semaine-03/exercice-tab/script.js
@@ -1,95 +1,43 @@
-// const utilisateurs = [
-//   { nom: "Alice", age: 17 },
-//   { nom: "Bob", age: 25 },
-//   { nom: "Charlie", age: 30 },
-// ];
+const listes = document.getElementById("listeUtilisateurs");
 
-// utilisateurs.forEach((item) => {
-//   console.log(item.nom);
-// });
-
-// const nouvelleListe = utilisateurs.filter(
-//   (personneMajeur) => personneMajeur.age > 18
-// );
-
-// console.log(nouvelleListe);
-
-// const nomEnMajuscule = utilisateurs.map(
-//   (item) => `${item.nom} à ${item.age} ans !`
-// );
-
-// console.log(nomEnMajuscule);
-
-// const produits = [
-//   { nom: "Stylo", prix: 1.5, disponible: true },
-//   { nom: "Cahier", prix: 3.0, disponible: false },
-//   { nom: "Gomme", prix: 1.9, disponible: false },
-//   { nom: "Effaceur", prix: 2.1, disponible: true },
-//   { nom: "Règle", prix: 2.4, disponible: true },
-//   { nom: "Livre", prix: 2.8, disponible: true },
-//   { nom: "Trousse", prix: 2.6, disponible: false },
-// ];
-
-// let taxer = [];
-
-// produits.forEach((element) => {
-//   const produit = element.prix * 1.2;
-//   taxer.push(produit);
-// });
-
-// console.log(taxer);
-
-// produits.forEach((element) => {
-//   console.log(`Produit: ${element.nom}`);
-// });
-
-// const produitDispo = produits.filter((element) => element.disponible === true);
-
-// console.log(produitDispo);
-
-// const produitTaxer = produits.map((element) => element.prix * 1.2);
-
-// console.log(produitTaxer);
-
-// function ajouterTva(produits) {
-//   let nouveauTableau = [];
-//   produits.forEach((element) => {
-//     const produit = element.prix * 1.2;
-//     nouveauTableau.push(produit);
-//   });
-//   return nouveauTableau;
-// }
-
-// const resultat = ajouterTva(produits);
-// console.log(resultat);
-
-// function ajouterTvaDetaillee(produits) {
-//   const tableauObj = [];
-
-//   produits.forEach((element) => {
-//     const produit = element.prix * 1.2;
-
-//     const obj = {
-//       nom: element.nom,
-//       prixOriginal: element.prix,
-//       prixTTC: produit.toFixed(2),
-//     };
-//     tableauObj.push(obj);
-//   });
-
-//   return tableauObj;
-// }
-
-// const resultat = ajouterTvaDetaillee(produits);
-
-// console.log(resultat);
-
-const produits = [
-  { nom: "Stylo", prix: 1.5, quantite: 430, disponible: true },
-  { nom: "Cahier", prix: 3.0, quantite: 220, disponible: false },
-  { nom: "Gomme", prix: 1.9, quantite: 350, disponible: false },
-  { nom: "Effaceur", prix: 2.1, quantite: 420, disponible: true },
-  { nom: "Règle", prix: 2.4, quantite: 300, disponible: true },
-  { nom: "Livre", prix: 2.8, quantite: 500, disponible: true },
-  { nom: "Trousse", prix: 2.6, quantite: 360, disponible: false },
+const utilisateurs = [
+  { nom: "Alice", email: "alice@mail.com", role: "admin", actif: true },
+  { nom: "Roger", email: "roger@mail.com", role: "user", actif: false },
+  { nom: "Steeve", email: "steeve@mail.com", role: "user", actif: true },
+  { nom: "Karim", email: "karim@mail.com", role: "admin", actif: true },
+  { nom: "Tanina", email: "tanina@mail.com", role: "user", actif: false },
 ];
+
+const head = document.createElement("thead");
+const ligne = document.createElement("tr");
+const colonne = document.createElement("th");
+
+const users = document.createElement("tbody");
+
+ligne.appendChild(colonne);
+head.appendChild(ligne);
+listes.appendChild(head);
+listes.appendChild(users);
+
+utilisateurs.forEach((utilisateur) => {
+  const ligne = document.createElement("tr");
+
+  const tdNom = document.createElement("td");
+  tdNom.textContent = `Nom: ${utilisateur.nom}`;
+
+  const tdEmail = document.createElement("td");
+  tdEmail.textContent = `Email: ${utilisateur.email}`;
+
+  const tdRole = document.createElement("td");
+  tdRole.textContent = `Role: ${utilisateur.role}`;
+
+  const tdActif = document.createElement("td");
+  tdActif.textContent = `Actif: ${utilisateur.actif ? "✅" : "❌"}`;
+
+  ligne.appendChild(tdNom);
+  ligne.appendChild(tdEmail);
+  ligne.appendChild(tdRole);
+  ligne.appendChild(tdActif);
+
+  users.appendChild(ligne);
+});
