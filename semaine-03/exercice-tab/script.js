f3b0896abff5bbb679fe780ec3580aa8dbf1cdb5
sedik <sedik91270@hotmail.fr>
2025-06-04 10:47:16 +0200

    amélioration du style du formulaire et correction du hover du bouton

--- semaine-03/exercice-tab/script.js
+++ semaine-03/exercice-tab/script.js
@@ -1,17 +1,17 @@
 const listes = document.getElementById("listeUtilisateurs");
 
 const utilisateurs = [
-  { nom: "Alice", email: "alice@mail.com", role: "admin", actif: true },
-  { nom: "Roger", email: "roger@mail.com", role: "user", actif: false },
-  { nom: "Steeve", email: "steeve@mail.com", role: "user", actif: true },
-  { nom: "Karim", email: "karim@mail.com", role: "admin", actif: true },
-  { nom: "Tanina", email: "tanina@mail.com", role: "user", actif: false },
+  { nom: "Alice", email: "alice@mail.com", statut: "admin", actif: true },
+  { nom: "Roger", email: "roger@mail.com", statut: "user", actif: false },
+  { nom: "Steeve", email: "steeve@mail.com", statut: "user", actif: true },
+  { nom: "Karim", email: "karim@mail.com", statut: "admin", actif: true },
+  { nom: "Tanina", email: "tanina@mail.com", statut: "user", actif: false },
 ];
 
 const head = document.createElement("thead");
 const ligne = document.createElement("tr");
 
-const titres = ["Nom", "Email", "Rôle", "Actif"];
+const titres = ["Nom", "Email", "Statut", "Actif"];
 
 titres.forEach((titre) => {
   const colonne = document.createElement("th");
@@ -34,15 +34,17 @@ utilisateurs.forEach((utilisateur) => {
   const tdEmail = document.createElement("td");
   tdEmail.textContent = `${utilisateur.email}`;
 
-  const tdRole = document.createElement("td");
-  tdRole.textContent = `${utilisateur.role}`;
+  const tdStatut = document.createElement("td");
+  tdStatut.textContent = `${
+    utilisateur.statut === "admin" ? "Administrateur" : "Utilisateur"
+  }`;
 
   const tdActif = document.createElement("td");
   tdActif.textContent = `${utilisateur.actif ? "✅" : "❌"}`;
 
   ligne.appendChild(tdNom);
   ligne.appendChild(tdEmail);
-  ligne.appendChild(tdRole);
+  ligne.appendChild(tdStatut);
   ligne.appendChild(tdActif);
 
   users.appendChild(ligne);
