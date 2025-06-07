635072133acea25708c2cd0db88b36743e1486a5
sedik <sedik91270@hotmail.fr>
2025-06-07 11:54:27 +0200

    Semaine-04 dynamiser avec javascript et structure de code

---
+++ semaine-04/filtre-tab/script.js
@@ -0,0 +1,7 @@
+const utilisateurs = [
+  { nom: "Alice", email: "alice@mail.com", statut: "admin", actif: true },
+  { nom: "Roger", email: "roger@mail.com", statut: "user", actif: false },
+  { nom: "Steeve", email: "steeve@mail.com", statut: "user", actif: true },
+  { nom: "Karim", email: "karim@mail.com", statut: "admin", actif: true },
+  { nom: "Tanina", email: "tanina@mail.com", statut: "user", actif: false },
+];
