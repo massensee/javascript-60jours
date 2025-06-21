5d082d09ff39ad2cab018c6df3cb241f998f3219
sedik <sedik91270@hotmail.fr>
2025-06-21 17:18:33 +0200

    refactor: création d’une fonction pour générer les éléments <li> des livres

--- semaine-06/book-tracker/app.js
+++ semaine-06/book-tracker/app.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("book-form");
 const result = document.getElementById("liste-livres");
 const livres = JSON.parse(localStorage.getItem("livres")) || [];
+afficherLivre();
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -29,24 +30,30 @@ function afficherLivre() {
   result.innerHTML = "";
 
   livres.forEach((livre, index) => {
-    const liEl = document.createElement("li");
-    liEl.textContent = `${livre.titre} - ${livre.auteur}`;
+    const liEl = creerElement(livre, index);
     result.appendChild(liEl);
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "X";
-    deleteBtn.dataset.index = index;
-    liEl.appendChild(deleteBtn);
-
-    deleteBtn.addEventListener("click", () => {
-      livres.splice(index, 1);
-      afficherLivre();
-    });
   });
-
-  return livres;
 }
 saveLivres();
+
+function creerElement(livre, index) {
+  const liEl = document.createElement("li");
+  liEl.textContent = `Livre: ${livre.titre} - ${livre.auteur}`;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.dataset.index = index;
+
+  deleteBtn.addEventListener("click", () => {
+    livres.splice(index, 1);
+    saveLivres();
+    afficherLivre();
+  });
+
+  liEl.appendChild(deleteBtn);
+
+  return liEl;
+}
 
 function saveLivres() {
   localStorage.setItem("livres", JSON.stringify(livres));
