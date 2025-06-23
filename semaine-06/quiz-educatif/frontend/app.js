ea26a3a63694cc2e6ab46361a959559e18bc8d9e
sedik <sedik91270@hotmail.fr>
2025-06-23 18:14:14 +0200

    Affichage de la première question et envoi de la réponse au backend via le frontend

--- semaine-06/quiz-educatif/frontend/app.js
+++ semaine-06/quiz-educatif/frontend/app.js
@@ -3,16 +3,42 @@ const container = document.getElementById("quiz-container");
 const API = "http://localhost:3000/questions";
 
 fetch(`${API}?t=${Date.now()}`)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Aucun API trouvé !");
-    }
-    return response.json();
-  })
+  .then((response) => response.json())
   .then((data) => {
-    console.clear(); // Efface la console pour que ce soit plus lisible
-    console.log("Questions reçues du backend :", data);
+    const question = data[0];
+
+    const questionTitle = document.createElement("h2");
+    questionTitle.textContent = question.question;
+    container.appendChild(questionTitle);
+
+    question.reponses.forEach((reponse, index) => {
+      const btn = document.createElement("button");
+      btn.textContent = reponse;
+
+      btn.addEventListener("click", () => {
+        envoyerReponse(0, index);
+      });
+
+      container.appendChild(btn);
+    });
   })
   .catch((error) => {
     console.error("Erreur lors de la récupération des données :", error);
   });
+
+function envoyerReponse(questionIndex, reponseIndex) {
+  fetch("http://localhost:3000/repondre", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ questionIndex, reponseIndex }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      alert(data.message);
+    })
+    .catch((error) => {
+      console.error("Erreur lors de l'envoie de la reponse :", error);
+    });
+}
