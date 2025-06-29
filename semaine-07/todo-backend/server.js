80f9c40a156e6fb47f75c1ea5bb3f723b760df78
sedik <sedik91270@hotmail.fr>
2025-06-29 16:36:10 +0200

    feat: mise à jour de l'état d'une tâche avec la méthode PATCH

--- semaine-07/todo-backend/server.js
+++ semaine-07/todo-backend/server.js
@@ -47,6 +47,28 @@ app.delete("/todos/:id", (req, res) => {
   res.json({ message: `Tâche avec id ${id} supprimée.` });
 });
 
+app.patch("/todos/:id", (req, res) => {
+  console.log("Body reçu :", req.body);
+  const id = parseInt(req.params.id);
+  const { fait } = req.body;
+
+  const todo = todos.find((t) => t.id === id);
+
+  if (!todo) {
+    return res.status(404).json({ message: "Tâche non trouvée." });
+  }
+
+  if (typeof fait !== "boolean") {
+    return res
+      .status(400)
+      .json({ message: "La valeur 'fait' doit être true ou false." });
+  }
+
+  todo.fait = fait;
+
+  res.json({ message: `Tâche ${id} mise à jour.`, todo });
+});
+
 app.listen(PORT, (req, res) => {
   console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
